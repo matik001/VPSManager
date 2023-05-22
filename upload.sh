ee24ee770e5463ea091157aa51348dcfaad234c0
Mateusz Kisiel <mateusz.kisiel.mk@gmail.com>
2023-05-22 02:36:36 +0200

    Added postgres

--- upload.sh
+++ upload.sh
@@ -1,5 +1,14 @@
+# NGINX
 rsync -e "ssh -o StrictHostKeyChecking=no" -arvc ./nginx root@157.230.18.69:~/config/
 
 ssh -o StrictHostKeyChecking=no root@157.230.18.69 "
     cd ~/config/nginx;
+    docker compose up --build --remove-orphans -d"
+
+# POSTGRES
+
+rsync -e "ssh -o StrictHostKeyChecking=no" -arvc ./postgres root@157.230.18.69:~/config/
+
+ssh -o StrictHostKeyChecking=no root@157.230.18.69 "
+    cd ~/config/postgres;
     docker compose up --build --remove-orphans -d"
