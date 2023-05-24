70c3115641bd1abf49692ba0506146140a1d8c2f
Matik <mateusz.kisiel.mk@gmail.com>
2023-05-24 19:49:01 +0200

    Added .env

--- upload.sh
+++ upload.sh
@@ -1,14 +1,18 @@
+ENV_VARS=$(cat .env | xargs)
+export $ENV_VARS
 # NGINX
-rsync -e "ssh -o StrictHostKeyChecking=no" -arvc ./nginx root@157.230.18.69:~/config/
+rsync -e "ssh -o StrictHostKeyChecking=no" -arvc ./nginx $SSH_USER@$SSH_IP:~/config/
 
-ssh -o StrictHostKeyChecking=no root@157.230.18.69 "
+ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_IP "
+    export $ENV_VARS;
     cd ~/config/nginx;
     docker compose up --build --remove-orphans -d"
 
 # POSTGRES
 
-rsync -e "ssh -o StrictHostKeyChecking=no" -arvc ./postgres root@157.230.18.69:~/config/
+rsync -e "ssh -o StrictHostKeyChecking=no" -arvc ./postgres $SSH_USER@$SSH_IP:~/config/
 
-ssh -o StrictHostKeyChecking=no root@157.230.18.69 "
+ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_IP "
+    export $ENV_VARS;
     cd ~/config/postgres;
     docker compose up --build --remove-orphans -d"
